37aa826d90d5bc2e8dfd3461c218b25c85d02354
joellepfernandez <joellepfernandez@gmail.com>
2018-04-13 14:43:32 +0000

    added database.php

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -6,9 +6,4 @@ define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_NAME", "gallery_db");
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-if($connection){
-    echo "true";
-}
 ?>
