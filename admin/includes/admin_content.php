00e5d3a1b1a228ea7a2dde1773479d265b0931b2
joellepfernandez <joellepfernandez@gmail.com>
2018-04-13 15:35:20 +0000

    updated  database.php with error messages

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,10 +9,13 @@
                         </h1>
                         
                         <?php
-                        if($database->connection){
-                            echo "true";
-                        }
+                            $sql = "SELECT * FROM users WHERE id=1";
+                            $result = $database->query($sql);
+                            $user_found = mysqli_fetch_array($result);
+                            
+                            echo $user_found['username'];
                         ?>
+                        
                         <ol class="breadcrumb">
                             <li>
                                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
