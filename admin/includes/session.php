8edeb0fce74ce9aa7d41cf52abe7fc4dc9e4eccd
joellepfernandez <joellepfernandez@gmail.com>
2018-04-17 14:01:15 +0000

    added is_signed_in, login, and loggout methods

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,11 +2,31 @@
 
 class Session {
     
-    private $signed_in;
+    private $signed_in = false;
     public $user_id;
     
     function __construct(){
         session_start();
+        $this->check_the_login();
+    }
+    
+    public function is_signed_in(){
+        return $this->signed_in;
+    } // getter method
+    
+    public function login($user){
+        if($user){
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+  
+    }
+    
+    public function loggout($user){
+        
+        unset($_SESSION['user_id'] );
+        unset($user->id);
+        $this->signed_in = false;
     }
     
     private function check_the_login(){
